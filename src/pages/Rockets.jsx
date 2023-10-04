d85e1549d712caddfd362da389a17cc8d28214dc
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-04 22:58:19 +0500

    linter issues resolved and styling navbar

--- src/pages/Rockets.jsx
+++ src/pages/Rockets.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Rockets = () => (
   <>
     <div className="rocketContainer">
-      <img src="./public/assets/planet.png" alt="Rocket" className="rocketImage" />
       <h2>Rockets</h2>
     </div>
   </>
