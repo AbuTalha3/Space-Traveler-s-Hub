5d173cee0830de842240d64530161fcd40996029
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-04 22:43:21 +0500

    remove linter errors

--- src/pages/Rockets.jsx
+++ src/pages/Rockets.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Rockets = () => (
-  <div>Rockets</div>
+  <>
+    <div className="rocketContainer">
+      <img src="./public/assets/planet.png" alt="Rocket" className="rocketImage" />
+      <h2>Rockets</h2>
+    </div>
+  </>
 );
 
 export default Rockets;
