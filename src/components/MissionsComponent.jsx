cd7ae73f521f37ed099c7c0587c050ab7a6e6ce1
Ismail mayito <ismayito1@gmail.com>
2023-10-05 09:57:53 +0300

    Fix linter errors

--- src/components/MissionsComponent.jsx
+++ src/components/MissionsComponent.jsx
@@ -6,7 +6,6 @@ import './Missions.css';
 const MissionsComponent = () => {
   const dispatch = useDispatch();
   const { missions } = useSelector((store) => store.missions);
-  console.log(missions);
 
   useEffect(() => {
     dispatch(fetchMissions());
@@ -21,16 +20,14 @@ const MissionsComponent = () => {
           <th style={{ width: '8%' }}>Status</th>
           <div style={{ width: '14%', backgroundColor: 'white' }} />
         </tr>
-        {missions.map(mission => {
-          return(
+        {missions.map((mission) => (
           <tr key={mission.mission_id}>
             <td style={{ width: '10%' }} className="name">{mission.mission_name}</td>
             <td style={{ width: '68%' }} className="description">{mission.description}</td>
             <td style={{ width: '8%' }} className="joinAction"><button type="button">Join Mission</button></td>
             <td style={{ width: '14%' }} className="leaveAction"><button type="button">Leave Mission</button></td>
           </tr>
-          )
-        })}
+        ))}
       </table>
     </div>
   );
