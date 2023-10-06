d578704557aa65be658f0019e3483284605b84eb
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-06 21:38:02 +0500

    Create my Profile

---
+++ src/components/RocketProfileComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './Profile.css';
+
+function RocketProfileComponent() {
+  const { rocketArr } = useSelector((store) => store.rockets);
+  const reservedRockets = rocketArr.filter((rockets) => rockets.reserved === true);
+  return (
+    <div className="missionsContainer">
+      {
+          reservedRockets.map((item) => (
+            <p key={item.id} className="mission">{item.name}</p>
+          ))
+      }
+    </div>
+  );
+}
+
+export default RocketProfileComponent;
