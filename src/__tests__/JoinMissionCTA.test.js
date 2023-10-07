47daf230815cee465e24d1bc7e235e438beab075
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-10-07 11:10:25 +0500

    Add tests for react components

---
+++ src/__tests__/JoinMissionCTA.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
+import JoinMissionCTA from '../components/JoinMissionCTA';
+import { joinMission } from '../components/Redux/Missions/missionsSlice';
+
+// Mock useDispatch and joinMission
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(),
+}));
+jest.mock('../components/Redux/Missions/missionsSlice', () => ({
+  joinMission: jest.fn(),
+}));
+
+describe('JoinMissionCTA', () => {
+  const mockDispatch = jest.fn();
+
+  beforeEach(() => {
+    useDispatch.mockReturnValue(mockDispatch);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('dispatches joinMission action when the button is clicked', () => {
+    const missionId = '123';
+    const { getByText } = render(<JoinMissionCTA id={missionId} />);
+    const joinMissionButton = getByText('Join Mission');
+    fireEvent.click(joinMissionButton);
+
+    expect(mockDispatch).toHaveBeenCalledWith(joinMission(missionId));
+  });
+});
---
+++ src/__tests__/LeaveMissionCTA.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { useDispatch } from 'react-redux';
+import LeaveMissionCTA from '../components/LeaveMissionCTA';
+import { cancelMission } from '../components/Redux/Missions/missionsSlice';
+
+// Mock useDispatch and joinMission
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: jest.fn(),
+}));
+jest.mock('../components/Redux/Missions/missionsSlice', () => ({
+  cancelMission: jest.fn(),
+}));
+
+describe('JoinMissionCTA', () => {
+  const mockDispatch = jest.fn();
+
+  beforeEach(() => {
+    useDispatch.mockReturnValue(mockDispatch);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('dispatches joinMission action when the button is clicked', () => {
+    const missionId = '123';
+    const { getByText } = render(<LeaveMissionCTA id={missionId} />);
+    const leaveMissionButton = getByText('Leave Mission');
+    fireEvent.click(leaveMissionButton);
+
+    expect(mockDispatch).toHaveBeenCalledWith(cancelMission(missionId));
+  });
+});
